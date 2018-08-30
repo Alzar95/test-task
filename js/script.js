1827f311557c4badae47eab8ceb7263c8381ab96
Pavel Nishcharetau <alzarlion@gmail.com>
2018-08-30 17:50:43 +0300

    New update

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 let dataList = document.createElement("ol");
-let listElement;
+let listElement, allData;
 dataList.setAttribute("id", "my-list");
 
 function getData() {
@@ -7,6 +7,7 @@ function getData() {
         .then(response => {
                 response.json().then(data => {
                     let elementList;
+                    allData = data;
 
                     for(let i = 0; i < data.data.length; i++) {
                         elementList = document.createElement("li");
@@ -21,66 +22,56 @@ function getData() {
 }
 
 function filterByLengthOfWords() {
-    if(document.getElementById("txt").value !== '') {
+    if (document.getElementById("txt").value !== '') {
         let emptyList = document.getElementById("my-list");
         emptyList.innerHTML = '';
         let elementList;
 
-        fetch('http://www.mrsoft.by/data.json')
-            .then(response => {
-                response.json().then(data => {
-                    let elementList;
-
-                    for(let i = 0; i < data.data.length; i++) {
-                        if(document.getElementById("txt").value < data.data[i].length) {
-                            elementList = document.createElement("li");
-                            listElement = document.createTextNode(data.data[i]);
-                            elementList.appendChild(listElement);
-                            document.getElementById("my-list").appendChild(elementList);
-                        }
-                    }
-                });
-            });
-
-
+        for (let i = 0; i < allData.data.length; i++) {
+            if (document.getElementById("txt").value < allData.data[i].length) {
+                elementList = document.createElement("li");
+                listElement = document.createTextNode(allData.data[i]);
+                elementList.appendChild(listElement);
+                document.getElementById("my-list").appendChild(elementList);
+            }
+        }
 
         document.body.appendChild(dataList);
     }
 }
 
 function substringFilter() {
-    if(document.getElementById("txt").value !== '') {
+    if (document.getElementById("txt").value !== '') {
         let emptyList = document.getElementById("my-list");
         let strValue = '' + document.getElementById("txt").value;
         emptyList.innerHTML = '';
+        let elementList;
 
-        fetch('http://www.mrsoft.by/data.json')
-            .then(response => {
-                response.json().then(data => {
-                    let elementList;
-
-                    for(let i = 0; i < data.data.length; i++) {
-                        if(document.getElementById("checkForRegister").checked === true) {
-                            if (data.data[i].indexOf(strValue) !== -1) {
-                                elementList = document.createElement("li");
-                                listElement = document.createTextNode(data.data[i]);
-                                elementList.appendChild(listElement);
-                                document.getElementById("my-list").appendChild(elementList);
-                            }
-                        } else {
-                            if (data.data[i].toUpperCase().indexOf(strValue.toUpperCase()) !== -1) {
-                                elementList = document.createElement("li");
-                                listElement = document.createTextNode(data.data[i]);
-                                elementList.appendChild(listElement);
-                                document.getElementById("my-list").appendChild(elementList);
-                            }
-                        }
-                    }
-                });
-            });
+        for (let i = 0; i < allData.data.length; i++) {
+            if (document.getElementById("checkForRegister").checked === true) {
+                if (allData.data[i].indexOf(strValue) !== -1) {
+                    elementList = document.createElement("li");
+                    listElement = document.createTextNode(allData.data[i]);
+                    elementList.appendChild(listElement);
+                    document.getElementById("my-list").appendChild(elementList);
+                }
+            } else {
+                if (allData.data[i].toUpperCase().indexOf(strValue.toUpperCase()) !== -1) {
+                    elementList = document.createElement("li");
+                    listElement = document.createTextNode(allData.data[i]);
+                    elementList.appendChild(listElement);
+                    document.getElementById("my-list").appendChild(elementList);
+                }
+            }
+        }
 
         document.body.appendChild(dataList);
     }
 }
+
+let el1 = document.getElementById("filter-by-length-of-words");
+el1.addEventListener("click", filterByLengthOfWords, false);
+let el2 = document.getElementById("substring-filter");
+el2.addEventListener("click", substringFilter, false);
 
 getData();
